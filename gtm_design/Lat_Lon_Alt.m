8e4c9cb18a8b798eb64bb5eea4e2174f8cd76201
Furkan <ciftlikcifurkan@gmail.com>
2024-01-25 16:57:33 +0300

    v0.0.8
    AÇIKLAMA:
    - İniş performansının iyileştirilmesi için boylamsal kontrolcü hızlandırıldı.

---
+++ gtm_design/Lat_Lon_Alt.m
@@ -0,0 +1,24 @@
+% Convert from lat/lon to ft to meter(Calibrated at Smithfield)
+%% Run for once
+sim_lon(1:10000) = [];
+sim_lat(1:10000) = [];
+sim_alt(1:10000) = [];
+
+%%
+
+dist_lon = sim_lon - sim_lon(1);
+dist_lat = sim_lat - sim_lat(1);
+
+dist_lat= dist_lat * 180/pi * 364100.79 * 0.3048;
+dist_lon= dist_lon * 180/pi * 291925.24 * 0.3048;
+
+% Convert the altitude,ft to meter
+alt= sim_alt * 0.3048;
+
+figure;
+plot3(dist_lon,dist_lat,alt,"black","LineWidth",2,"Marker",".")
+title("Longitude - Latitude - Altitude");
+xlabel("Longitude(m)");
+ylabel("Latitude(m)");
+zlabel("Altitude(m)");
+grid on
